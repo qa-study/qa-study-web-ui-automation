109d6c8f4c5d0ef570ac87cc717cb2e8a2887774
유승훈 <a5750791@gmail.com>
2024-11-23 16:19:26 +0900

    [Feat] 탭 내비게이션 테스트 케이스 추가

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,20 +1,24 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 
 class BasePage:
-
     def __init__(self, driver):
         self.driver = driver
 
+    def driver_title(self, expected_title):
+        actual_title = self.driver.title
+        assert actual_title == expected_title
+
     def click_element(self, selector, locator):
+        self.driver.implicitly_wait(10)
         element = self.driver.find_element(by=selector, value=locator)
         element.click()
 
     def input_element(self, selector, locator, text):
+        self.driver.implicitly_wait(10)
         element = self.driver.find_element(by=selector, value=locator)
         element.send_keys(text)
 
     def is_displayed(self, selector, locator):
+        self.driver.implicitly_wait(10)
         element = self.driver.find_element(by=selector, value=locator)
-        element.is_displayed()
+        return element.is_displayed()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
 import pytest
+from selenium import webdriver
 
 
 @pytest.fixture(scope="function")
 def driver():
+    # Chrome WebDriver 초기화
     driver = webdriver.Chrome()
     driver.get("https://comic.naver.com/index")
     driver.implicitly_wait(5)
